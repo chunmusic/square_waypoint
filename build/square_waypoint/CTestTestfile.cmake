0d93805e24a0a312f6944785b147283a36811919
Pongsakorn <chun_ps@hotmail.com>
2021-10-05 16:34:58 +0900

    remove devel build

--- build/square_waypoint/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pongsakorn/square_waypoint/src/square_waypoint
-# Build directory: /home/pongsakorn/square_waypoint/build/square_waypoint
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
