2b1e88f8eb1117a4bb7567362ec1400c00e0df3a
Pongsakorn <chun_ps@hotmail.com>
2021-10-11 23:30:01 +0900

    update qav250 script

--- src/square_waypoint/src/qav250.cpp
+++ src/square_waypoint/src/qav250.cpp
@@ -7,29 +7,18 @@
 #include <mavros_msgs/ParamSet.h>
 
 #include <iostream>
+#include <future>
 
 using namespace std;
 
 mavros_msgs::State uav0_current_state;
-
 
 void uav0_state_callback(const mavros_msgs::State::ConstPtr& msg){
 
     uav0_current_state = *msg;
 }
 
-
-int main (int argc, char **argv) 
-{
-
-    ros::init(argc, argv, "one_unit");
-    ros::NodeHandle nh; 
-
-    ros::Subscriber uav0_state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, uav0_state_callback); // 10 is buffer size
-    ros::Publisher uav0_local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
-
-    ros::ServiceClient uav0_arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
-    ros::ServiceClient uav0_set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+void check_connection(){
 
     ros::Rate rate(20.0);
 
@@ -39,33 +28,43 @@ int main (int argc, char **argv)
         rate.sleep();
     }
 
+}
+
+bool send_position(int iter, ros::Publisher uav_loc, float pos_x, float pos_y, float pos_z, float rot_x, float rot_y, float rot_z, float rot_w){
+    
+    ros::Rate rate(20.0);
+
     geometry_msgs::PoseStamped uav0_pose;
 
+    uav0_pose.pose.position.x = pos_x;
+    uav0_pose.pose.position.y = pos_y;
+    uav0_pose.pose.position.z = pos_z;
+    uav0_pose.pose.orientation.x = rot_x;
+    uav0_pose.pose.orientation.y = rot_y;
+    uav0_pose.pose.orientation.z = rot_z;
+    uav0_pose.pose.orientation.w = rot_w;
 
-    uav0_pose.pose.position.x = 0;
-    uav0_pose.pose.position.y = 0;
-    uav0_pose.pose.position.z = 0.5;
-    uav0_pose.pose.orientation.x =  0;
-    uav0_pose.pose.orientation.y =  0;
-    uav0_pose.pose.orientation.z =  0.707;
-    uav0_pose.pose.orientation.w =  0.707;
+    for (int i = iter; ros::ok() && i > 0; --i){
 
-    for (int i = 20; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
+        uav_loc.publish(uav0_pose);
 
         ros::spinOnce();
         rate.sleep();
         ROS_INFO("Iteration: %d",i);
     }
 
+}
+
+void set_offboard(ros::ServiceClient uav0_set_mode_client, ros::ServiceClient uav0_arming_client, ros::Publisher uav0_local_pos_pub
+                    ){
+
     mavros_msgs::SetMode uav0_offb_set_mode;
     uav0_offb_set_mode.request.custom_mode = "OFFBOARD";
     mavros_msgs::CommandBool uav0_arm_cmd;
     uav0_arm_cmd.request.value = true;
 
+    ros::Rate rate(20.0);
     ros::Time last_request = ros::Time::now();
-
 
     while (ros::ok()){
 
@@ -93,148 +92,50 @@ int main (int argc, char **argv)
 
         ros::spinOnce();
         rate.sleep();
+
+        geometry_msgs::PoseStamped uav0_pose;
+
+        uav0_pose.pose.position.x = 0;
+        uav0_pose.pose.position.y = 0;
+        uav0_pose.pose.position.z = 0.5;
+        uav0_pose.pose.orientation.x = 0;
+        uav0_pose.pose.orientation.y = 0;
+        uav0_pose.pose.orientation.z = 0.707;
+        uav0_pose.pose.orientation.w = 0.707;
         uav0_local_pos_pub.publish(uav0_pose);
 
     } 
 
+}
 
+int main (int argc, char **argv) 
+{
+
+    ros::init(argc, argv, "one_unit");
+    ros::NodeHandle nh; 
+
+    ros::Subscriber uav0_state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, uav0_state_callback); // 10 is buffer size
+    ros::Publisher uav0_local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+
+    ros::ServiceClient uav0_arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient uav0_set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+
+    check_connection();
+
+    send_position(20, uav0_local_pos_pub , 0, 0, 0.5, 0, 0 , 0.707, 0.707);
+
+    set_offboard(uav0_set_mode_client, uav0_arming_client, uav0_local_pos_pub);
     
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iteration: %d",i);
-
-    }
-
-    uav0_pose.pose.position.x = 0;
-    uav0_pose.pose.position.y = 1;
-    uav0_pose.pose.position.z = 0.5;
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-
-    }
-
-    uav0_pose.pose.orientation.x =  0;
-    uav0_pose.pose.orientation.y =  0;
-    uav0_pose.pose.orientation.z =  1;
-    uav0_pose.pose.orientation.w =  0;
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-        
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
-
-    uav0_pose.pose.position.x = -1;
-    uav0_pose.pose.position.y = 1;
-    uav0_pose.pose.position.z = 0.5;
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
-
-    uav0_pose.pose.orientation.x =  0;
-    uav0_pose.pose.orientation.y =  0;
-    uav0_pose.pose.orientation.z =  -0.707;
-    uav0_pose.pose.orientation.w =  0.707;
-
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-        
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
-
-    uav0_pose.pose.position.x = -1;
-    uav0_pose.pose.position.y = 0;
-    uav0_pose.pose.position.z = 0.5;
-
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-        
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
-
-    uav0_pose.pose.orientation.x =  0;
-    uav0_pose.pose.orientation.y =  0;
-    uav0_pose.pose.orientation.z =  0;
-    uav0_pose.pose.orientation.w =  1;
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
-
-    uav0_pose.pose.position.x = 0;
-    uav0_pose.pose.position.y = 0;
-    uav0_pose.pose.position.z = 0.5;
-
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-        
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
-
-    uav0_pose.pose.orientation.x =  0;
-    uav0_pose.pose.orientation.y =  0;
-    uav0_pose.pose.orientation.z =  0.707;
-    uav0_pose.pose.orientation.w =  0.707;
-
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-        
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
-
-    // landing
-    uav0_pose.pose.position.x = 0;
-    uav0_pose.pose.position.y = 0;
-    uav0_pose.pose.position.z = -0.5;
-
-    for (int i = 200; ros::ok() && i > 0; --i){
-
-        uav0_local_pos_pub.publish(uav0_pose);
-        
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("Iterartion: %d",i);
-    }
+    send_position(200   , uav0_local_pos_pub , 0, 0, 0.5, 0, 0 , 0.707, 0.707);
+    send_position(200, uav0_local_pos_pub , 0, 2, 0.5, 0, 0 , 0.707, 0.707);
+    send_position(200, uav0_local_pos_pub , 0, 2, 0.5, 0, 0 , 1, 0);
+    send_position(200, uav0_local_pos_pub , -2, 2, 0.5, 0, 0 , 1, 0);
+    send_position(200, uav0_local_pos_pub , -2, 2, 0.5, 0, 0 , -0.707, 0.707);
+    send_position(200, uav0_local_pos_pub , -2, 0, 0.5, 0, 0 , -0.707, 0.707);
+    send_position(200, uav0_local_pos_pub , -2, 0, 0.5, 0, 0 , 0, 1);
+    send_position(200, uav0_local_pos_pub , 0, 0, 0.5, 0, 0 , 0, 1);
+    send_position(200, uav0_local_pos_pub , 0, 0, 0.5, 0, 0 , 0.707, 0.707);
+    send_position(200, uav0_local_pos_pub , 0, 0, -0.5, 0, 0 , 0.707, 0.707);
 
     return 0;
 
